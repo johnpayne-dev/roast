ce04db6e3eb5cc6282138620d2d84f073e819927
Karl Velazquez <kvelaz@mit.edu>
2024-02-28 17:42:54 -0500

    Defined structs for symbol table, methods table, and fields table

--- src/semantics/symbol_table.h
+++ src/semantics/symbol_table.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include <glib.h>
+#include <stdbool.h>
+
+struct symbol_table {
+	struct symbol_table *parent;
+	GHashTable *method_table;
+	GHashTable *fields_table;
+};
+
+enum data_type { DATA_TYPE_INT, DATA_TYPE_BOOL, DATA_TYPE_VOID };
+
+struct method_descriptor {
+	bool imported;
+	enum data_type return_type;
+	GArray *arguments;
+};
+
+struct argument_descriptor {
+	enum data_type type;
+	char *identifier;
+};
+
+struct field_descriptor {
+	enum data_type type;
+	bool constant;
+	bool array;
+	size_t array_length;
+	GArray *initializers;
+};
