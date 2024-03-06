6fc88dc4b8f74060e32e84be8c2c7105a201b802
Karl Velazquez <kvelaz@mit.edu>
2024-03-06 11:26:45 -0500

    symbol table now has option to climb up scopes when getting/looking up an ident ifier

--- src/semantics/symbol_table.c
+++ src/semantics/symbol_table.c
@@ -21,8 +21,9 @@ symbol_table_t *symbol_table_get_parent(symbol_table_t *symbols)
 	return symbols->parent;
 }
 
-void symbol_table_add_method(symbol_table_t *symbols, char *method_identifier,
-			     struct ir_method *method)
+void symbol_table_declare_method(symbol_table_t *symbols,
+				 char *method_identifier,
+				 struct ir_method *method)
 {
 	g_assert(method_identifier != NULL || method_identifier[0] != '\0');
 	g_assert(method != NULL);
@@ -31,14 +32,23 @@ void symbol_table_add_method(symbol_table_t *symbols, char *method_identifier,
 }
 
 struct ir_method *symbol_table_get_method(symbol_table_t *symbols,
-					  char *method_identifier)
+					  char *method_identifier, bool climb)
 {
 	g_assert(method_identifier != NULL || method_identifier[0] != '\0');
-	return g_hash_table_lookup(symbols->methods_table, method_identifier);
+	symbol_table_t *current_scope = symbols;
+	struct ir_method *method = NULL;
+	while (((method = g_hash_table_lookup(current_scope->methods_table,
+					      method_identifier)) == NULL) &&
+	       climb) {
+		current_scope = symbol_table_get_parent(current_scope);
+		if (current_scope == NULL)
+			break;
+	}
+	return method;
 }
 
-void symbol_table_add_field(symbol_table_t *symbols, char *field_identifier,
-			    struct ir_field *field)
+void symbol_table_declare_field(symbol_table_t *symbols, char *field_identifier,
+				struct ir_field *field)
 {
 	g_assert(field_identifier != NULL || field_identifier[0] != '\0');
 	g_assert(field != NULL);
@@ -47,10 +57,19 @@ void symbol_table_add_field(symbol_table_t *symbols, char *field_identifier,
 }
 
 struct ir_field *symbol_table_get_field(symbol_table_t *symbols,
-					char *field_identifier)
+					char *field_identifier, bool climb)
 {
 	g_assert(field_identifier != NULL || field_identifier[0] != '\0');
-	return g_hash_table_lookup(symbols->fields_table, field_identifier);
+	symbol_table_t *current_scope = symbols;
+	struct ir_field *field = NULL;
+	while (((field = g_hash_table_lookup(current_scope->fields_table,
+					     field_identifier)) == NULL) &&
+	       climb) {
+		current_scope = symbol_table_get_parent(current_scope);
+		if (current_scope == NULL)
+			break;
+	}
+	return field;
 }
 
 void symbol_table_free(symbol_table_t *symbols)
--- src/semantics/symbol_table.h
+++ src/semantics/symbol_table.h
@@ -12,16 +12,17 @@ symbol_table_t *symbol_table_new(struct symbol_table *parent_table);
 
 symbol_table_t *symbol_table_get_parent(symbol_table_t *symbols);
 
-void symbol_table_add_method(symbol_table_t *symbols, char *method_identifier,
-			     struct ir_method *method);
+void symbol_table_declare_method(symbol_table_t *symbols,
+				 char *method_identifier,
+				 struct ir_method *method);
 
 struct ir_method *symbol_table_get_method(symbol_table_t *symbols,
-					  char *method_identifier);
+					  char *method_identifier, bool climb);
 
-void symbol_table_add_field(symbol_table_t *symbols, char *field_identifier,
-			    struct ir_field *field);
+void symbol_table_declare_field(symbol_table_t *symbols, char *field_identifier,
+				struct ir_field *field);
 
 struct ir_field *symbol_table_get_field(symbol_table_t *symbols,
-					char *field_identifier);
+					char *field_identifier, bool climb);
 
 void symbol_table_free(symbol_table_t *symbols);
