8809222a5ac7932040d2db2fe626be57db6a8723
Karl Velazquez <kvelaz@mit.edu>
2024-04-05 16:04:18 -0400

    llir_node_new_method

--- src/assembly/llir.c
+++ src/assembly/llir.c
@@ -33,10 +33,19 @@ struct llir_node *llir_node_new_program(struct ir_program *program)
 	return NULL;
 }
 
-struct llir_node *llir_node_new_method(struct ir_method *method)
+struct llir_node *llir_node_new_method(struct ir_method *ir_method)
 {
-	g_assert(!"TODO");
-	return NULL;
+	g_assert(ir_method->imported == false);
+
+	struct llir_node *node = g_new(struct llir_node, 1);
+
+	node->type = LLIR_NODE_TYPE_METHOD;
+
+	node->method = llir_method_new(ir_method);
+
+	node->next = llir_node_new_block(ir_method->block);
+
+	return node;
 }
 
 struct llir_node *llir_node_new_field(struct ir_field *field)
@@ -60,4 +69,30 @@ struct llir_node *llir_node_new_instructions(struct ir_statement *statement)
 void llir_node_free(struct llir_node *node)
 {
 	g_assert(!"TODO");
+}
+
+struct llir_method *llir_method_new(struct ir_method *ir_method)
+{
+	struct llir_method *method = g_new(struct llir_method, 1);
+
+	method->identifier = g_strdup(ir_method->identifier);
+
+	method->arguments =
+		g_array_new(false, false, sizeof(struct llir_field *));
+
+	return method;
+}
+
+void llir_method_free(struct llir_method *method)
+{
+	g_free(method->identifier);
+
+	for (uint32_t i = 0; i < method->arguments->len; i++) {
+		struct llir_field *field = g_array_index(
+			method->arguments, struct llir_field *, i);
+		llir_field_free(field);
+	}
+	g_array_free(method->arguments, true);
+
+	g_free(method);
 }
