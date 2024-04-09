3f9a86a8ef4c7f7734e9e301a79113238deb2a4a
John Payne <johnpayn@mit.edu>
2024-04-09 19:18:08 -0400

    symbol_table.c/h

---
+++ src/assembly/symbol_table.h
@@ -0,0 +1,18 @@
+#include <glib.h>
+
+struct symbol_table {
+	GArray *tables;
+};
+
+struct symbol_table *symbol_table_new(void);
+
+void symbol_table_set(struct symbol_table *symbol_table, char *identifier,
+		      void *data);
+
+void *symbol_table_get(struct symbol_table *symbol_table, char *identifier);
+
+void symbol_table_push_scope(struct symbol_table *symbol_table);
+
+void symbol_table_pop_scope(struct symbol_table *symbol_table);
+
+void symbol_table_free(struct symbol_table *symbol_table);
