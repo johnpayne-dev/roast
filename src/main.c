3b59f97f83017ddd9db2655584584412ac40924d
John Payne <johnpayn@mit.edu>
2024-04-17 19:33:11 -0400

    implement optimization parsing

--- src/main.c
+++ src/main.c
@@ -8,16 +8,98 @@
 #include "assembly/assembly.h"
 #include "assembly/code_generator.h"
 
-struct options {
-	char *target;
-	char *output_file;
-	bool debug;
+enum target {
+	TARGET_SCAN,
+	TARGET_PARSE,
+	TARGET_INTER,
+	TARGET_ASSEMBLY,
 };
 
-static int parse_options(int *argc, char ***argv, struct options *options)
+enum optimzation {
+	OPTIMIZATION_CSE = 1 << 1,
+	OPTIMIZATION_CP = 1 << 2,
+	OPTIMIZATION_DCE = 1 << 3,
+	OPTIMIZATION_ALL = ~0,
+};
+
+struct options {
+	enum target target;
+	char *output_file;
+	enum optimzation optimizations;
+	bool debug;
+	char *input_file;
+};
+
+static void free_options(struct options *options)
+{
+	g_free(options->output_file);
+	g_free(options->input_file);
+}
+
+static int parse_target(char *target, struct options *options)
+{
+	if (target == NULL) {
+		options->target = TARGET_ASSEMBLY;
+	} else if (g_strcmp0(target, "scan") == 0) {
+		options->target = TARGET_SCAN;
+	} else if (g_strcmp0(target, "parse") == 0) {
+		options->target = TARGET_PARSE;
+	} else if (g_strcmp0(target, "inter") == 0) {
+		options->target = TARGET_INTER;
+	} else if (g_strcmp0(target, "assembly") == 0) {
+		options->target = TARGET_ASSEMBLY;
+	} else {
+		g_printerr("Unknown target '%s' passed in as option.\n",
+			   target);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int parse_optimizations(char *optimizations, struct options *options)
+{
+	options->optimizations = 0;
+
+	if (optimizations == NULL)
+		return 0;
+
+	char **optimization_list = g_strsplit(optimizations, ",", -1);
+	for (uint32_t i = 0; optimization_list[i] != NULL; i++) {
+		char *optimization = optimization_list[i];
+		if (g_strcmp0(optimization, "cse") == 0)
+			options->optimizations |= OPTIMIZATION_CSE;
+		else if (g_strcmp0(optimization, "-cse") == 0)
+			options->optimizations &= ~OPTIMIZATION_CSE;
+		else if (g_strcmp0(optimization, "cp") == 0)
+			options->optimizations |= OPTIMIZATION_CP;
+		else if (g_strcmp0(optimization, "-cp") == 0)
+			options->optimizations &= ~OPTIMIZATION_CP;
+		else if (g_strcmp0(optimization, "dce") == 0)
+			options->optimizations |= OPTIMIZATION_DCE;
+		else if (g_strcmp0(optimization, "-dce") == 0)
+			options->optimizations &= ~OPTIMIZATION_DCE;
+		else if (g_strcmp0(optimization, "all") == 0)
+			options->optimizations |= OPTIMIZATION_ALL;
+		else if (g_strcmp0(optimization, "-all") == 0)
+			options->optimizations &= ~OPTIMIZATION_ALL;
+		else {
+			g_printerr("Unknown optimizations '%s' passed in",
+				   optimization);
+			g_free(optimization_list);
+			return -1;
+		}
+	}
+
+	g_free(optimization_list);
+	return 0;
+}
+
+static int parse_options(int argc, char **argv, struct options *options)
 {
 	char *target = NULL;
 	char *output_file = NULL;
+	char *optimizations = NULL;
 	bool debug = false;
 
 	const GOptionEntry option_entries[] = {
@@ -28,7 +110,7 @@ static int parse_options(int *argc, char ***argv, struct options *options)
 			.arg = G_OPTION_ARG_STRING,
 			.arg_data = (void *)&target,
 			.description =
-				"<stage> is one of scan, parse, inter, or assembly.",
+				"<stage> is one of 'scan', 'parse', 'inter', or 'assembly'.",
 			.arg_description = "<stage>",
 		},
 		{
@@ -39,6 +121,16 @@ static int parse_options(int *argc, char ***argv, struct options *options)
 			.arg_data = (void *)&output_file,
 			.description = "Writes output to <output>",
 			.arg_description = "<output>",
+		},
+		{
+			.long_name = "optimizations",
+			.short_name = 'O',
+			.flags = 0,
+			.arg = G_OPTION_ARG_STRING,
+			.arg_data = (void *)&optimizations,
+			.description =
+				"<optimization> is one of 'cse', 'cp', 'dce' or 'all'.",
+			.arg_description = "<optimization>,...",
 		},
 		{
 			.long_name = "debug",
@@ -55,25 +147,36 @@ static int parse_options(int *argc, char ***argv, struct options *options)
 	GOptionContext *context = g_option_context_new("- decaf compiler.");
 	g_option_context_add_main_entries(context, option_entries, NULL);
 
+	int result = 0;
+
 	GError *error = NULL;
-	if (!g_option_context_parse(context, argc, argv, &error)) {
+	if (!g_option_context_parse(context, &argc, &argv, &error)) {
 		g_printerr("Failed to parse options: %s\n", error->message);
-		g_free(target);
-		g_free(output_file);
-		g_option_context_free(context);
 		return -1;
 	}
 
-	options->target = target;
+	if (argc != 2) {
+		g_printerr(argc < 2 ? "No input file passed in.\n" :
+				      "Too many input files passed in.\n");
+		options->input_file = NULL;
+		result = -1;
+	} else {
+		options->input_file = g_strdup(argv[1]);
+	}
+
 	options->output_file = output_file;
 	options->debug = debug;
-	return 0;
-}
 
-static void free_options(struct options *options)
-{
-	g_free(options->target);
-	g_free(options->output_file);
+	if (parse_target(target, options) != 0)
+		result = -1;
+
+	if (parse_optimizations(optimizations, options) != 0)
+		result = -1;
+
+	g_free(target);
+	g_free(optimizations);
+	g_option_context_free(context);
+	return result;
 }
 
 static int set_output_file(char *output_file)
@@ -86,16 +189,10 @@ static int set_output_file(char *output_file)
 	return 0;
 }
 
-static int get_input_file_contents(int argc, char *argv[], char **file_contents)
+static int get_input_file_contents(char *input_file, char **file_contents)
 {
-	if (argc != 2) {
-		g_printerr(argc < 2 ? "No input file passed in.\n" :
-				      "Too many input files passed in.\n");
-		return -1;
-	}
-
 	GError *error = NULL;
-	if (!g_file_get_contents(argv[1], file_contents, NULL, &error)) {
+	if (!g_file_get_contents(input_file, file_contents, NULL, &error)) {
 		g_printerr("%s\n", error->message);
 		return -1;
 	}
@@ -166,22 +263,22 @@ static int run_assembly_target(char *file_name, char *source)
 	return 0;
 }
 
-static int run_target(char *target, char *file_name, char *source)
+static int run_target(struct options *options, char *source)
 {
-	if (target == NULL)
-		return run_assembly_target(file_name, source);
-
-	if (g_strcmp0(target, "scan") == 0)
-		return run_scan_target(file_name, source, true, NULL);
-	if (g_strcmp0(target, "parse") == 0)
-		return run_parse_target(file_name, source, NULL);
-	if (g_strcmp0(target, "inter") == 0)
-		return run_intermediate_target(file_name, source, NULL);
-	if (g_strcmp0(target, "assembly") == 0)
-		return run_assembly_target(file_name, source);
-
-	g_printerr("Unknown target '%s' passed in as option.\n", target);
-	return -1;
+	switch (options->target) {
+	case TARGET_SCAN:
+		return run_scan_target(options->input_file, source, true, NULL);
+	case TARGET_PARSE:
+		return run_parse_target(options->input_file, source, NULL);
+	case TARGET_INTER:
+		return run_intermediate_target(options->input_file, source,
+					       NULL);
+	case TARGET_ASSEMBLY:
+		return run_assembly_target(options->input_file, source);
+	default:
+		g_assert(!"Unknown target");
+		return -1;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -189,16 +286,16 @@ int main(int argc, char *argv[])
 	struct options options = { 0 };
 	char *file_contents = NULL;
 
-	if (parse_options(&argc, &argv, &options) != 0)
+	if (parse_options(argc, argv, &options) != 0)
 		goto error_cleanup;
 
 	if (set_output_file(options.output_file) != 0)
 		goto error_cleanup;
 
-	if (get_input_file_contents(argc, argv, &file_contents) != 0)
+	if (get_input_file_contents(options.input_file, &file_contents) != 0)
 		goto error_cleanup;
 
-	if (run_target(options.target, argv[1], file_contents) != 0)
+	if (run_target(&options, file_contents) != 0)
 		goto error_cleanup;
 
 	return 0;
