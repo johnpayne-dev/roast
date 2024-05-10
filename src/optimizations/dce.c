edc2434f6769b2b7978f20ed349360e08ce6f8fe
Karl Velazquez <kvelaz@mit.edu>
2024-05-10 03:15:23 -0400

    suboptimal dce

--- src/optimizations/dce.c
+++ src/optimizations/dce.c
@@ -1,50 +1,95 @@
 #include "optimizations/dce.h"
 
-GArray *live_set = NULL;
+GHashTable *live_set = NULL;
+
+static GHashTable *live_set_init(void)
+{
+	return g_hash_table_new(g_str_hash, g_str_equal);
+}
+
+static void live_set_free(void)
+{
+	g_hash_table_destroy(live_set);
+}
+
+static uint32_t live_set_count(void)
+{
+	return g_hash_table_size(live_set);
+}
 
 static bool live_set_contains(char *variable)
 {
-	for (uint32_t i = 0; i < live_set->len; i++) {
-		if (g_array_index(live_set, char *, i) == variable) {
-			return true;
-		}
-	}
-	return false;
+	return g_hash_table_lookup(live_set, variable);
 }
 
 static void live_set_add(char *variable)
 {
-	if (!live_set_contains(variable))
-		g_array_append_val(live_set, variable);
+	if (!live_set_contains(variable)) {
+		g_hash_table_insert(live_set, variable, (gpointer) true);
+	}
+}
+
+void live_set_print_helper(gpointer key, gpointer value, gpointer user_data)
+{
+	g_print("%s,", (gchar *)key);
 }
 
 static void live_set_print(void)
 {
-	for (uint32_t i = 0; i < live_set->len; i++) {
-		g_print("%s,", g_array_index(live_set, char *, i));
-	}
+	g_hash_table_foreach(live_set, live_set_print_helper, NULL);
 	g_print("\n");
 }
 
-static void optimize_method(struct llir_iterator *iterator)
+static void get_live_variables_from_fields(struct llir_iterator *iterator)
 {
-	g_print("method\n");
+	for (uint32_t i = 0; i < iterator->block->fields->len; i++) {
+		struct llir_field *field = g_array_index(
+			iterator->block->fields, struct llir_field *, i);
+		if (field->is_array)
+			live_set_add(field->identifier);
+	}
 }
 
-static void optimize_block(struct llir_iterator *iterator)
+static void
+get_live_variables_from_method_call_arguments(struct llir_iterator *iterator)
 {
-	g_print("block\n");
-}
-
-static void optimize_assignment(struct llir_iterator *iterator)
-{
-	g_print("assignment\n");
 	if (iterator->assignment->type == LLIR_ASSIGNMENT_TYPE_METHOD_CALL)
-		live_set_add(iterator->assignment->destination);
+		for (u_int32_t i = 0; i < iterator->assignment->argument_count;
+		     i++) {
+			if (iterator->assignment->arguments[i].type ==
+			    LLIR_OPERAND_TYPE_FIELD)
+				live_set_add(iterator->assignment->arguments[i]
+						     .field);
+		}
+}
 
+static void get_live_variables_from_terminals(struct llir_iterator *iterator)
+{
+	switch (iterator->block->terminal_type) {
+	case LLIR_BLOCK_TERMINAL_TYPE_BRANCH:
+		if (iterator->block->branch->left.type ==
+		    LLIR_OPERAND_TYPE_FIELD)
+			live_set_add(iterator->block->branch->left.field);
+		if (iterator->block->branch->right.type ==
+		    LLIR_OPERAND_TYPE_FIELD)
+			live_set_add(iterator->block->branch->right.field);
+		break;
+	case LLIR_BLOCK_TERMINAL_TYPE_RETURN:
+		if (iterator->block->llir_return->source.type ==
+		    LLIR_OPERAND_TYPE_FIELD) {
+			live_set_add(
+				iterator->block->llir_return->source.field);
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+static void get_live_variables_from_assignments(struct llir_iterator *iterator)
+{
 	if (!live_set_contains(iterator->assignment->destination))
-		g_array_remove_index(iterator->block->assignments,
-				     iterator->assignment_index);
+		return;
 
 	switch (iterator->assignment->type) {
 	case LLIR_ASSIGNMENT_TYPE_MOVE:
@@ -101,34 +146,19 @@ static void optimize_assignment(struct llir_iterator *iterator)
 	}
 }
 
-static void optimize_terminal(struct llir_iterator *iterator)
+static void prune_dead_variables(struct llir_iterator *iterator)
 {
-	g_print("terminal\n");
-	switch (iterator->block->terminal_type) {
-	case LLIR_BLOCK_TERMINAL_TYPE_BRANCH:
-		if (iterator->block->branch->left.type ==
-		    LLIR_OPERAND_TYPE_FIELD)
-			live_set_add(iterator->block->branch->left.field);
-		if (iterator->block->branch->right.type ==
-		    LLIR_OPERAND_TYPE_FIELD)
-			live_set_add(iterator->block->branch->right.field);
-		break;
-	case LLIR_BLOCK_TERMINAL_TYPE_RETURN:
-		if (iterator->block->llir_return->source.type ==
-		    LLIR_OPERAND_TYPE_FIELD) {
-			live_set_add(
-				iterator->block->llir_return->source.field);
-		}
-		break;
-	default:
-		break;
+	if (!live_set_contains(iterator->assignment->destination) &&
+	    iterator->assignment->type != LLIR_ASSIGNMENT_TYPE_METHOD_CALL) {
+		g_array_remove_index(iterator->block->assignments,
+				     iterator->assignment_index);
+		iterator->assignment_index--;
 	}
 }
 
 void optimization_dead_code_elimination(struct llir *llir)
 {
-	// init live set
-	live_set = g_array_new(false, false, sizeof(char *));
+	live_set = live_set_init();
 
 	// add all global fields to live set
 	for (uint32_t i = 0; i < llir->fields->len; i++) {
@@ -136,10 +166,22 @@ void optimization_dead_code_elimination(struct llir *llir)
 				     ->identifier);
 	}
 
-	llir_iterate(llir, optimize_method, optimize_block, optimize_assignment,
-		     optimize_terminal, false);
+	// add terminal variables and method call arguments to live set
+	llir_iterate(llir, NULL, get_live_variables_from_fields,
+		     get_live_variables_from_method_call_arguments,
+		     get_live_variables_from_terminals, true);
 
-	live_set_print();
+	uint32_t prev_live_count = 0;
 
-	g_array_free(live_set, true);
+	do {
+		prev_live_count = live_set_count();
+		llir_iterate(llir, NULL, NULL,
+			     get_live_variables_from_assignments, NULL, true);
+	} while (live_set_count() != prev_live_count);
+
+	llir_iterate(llir, NULL, NULL, prune_dead_variables, NULL, true);
+
+	// live_set_print();
+
+	live_set_free();
 }
